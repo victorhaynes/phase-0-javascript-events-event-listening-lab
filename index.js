25da2aa03151d58077274acc5a1e18e06f4ccc8f
Victor Haynes <haynesvic@gmail.com>
2022-06-29 09:59:32 -0500

    commit tree 660ff4efbc0d945ffbd1d45a6460c61686b973c4

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-function addingEventListener() {
+// function addingEventListener() {
+//     const input = document.getElemenyById("button")
+//     input.addEventListener("Click",function({
+//         alert("You clicked the button")
+//     }))
+
+// }
+
+
+function addingEventListener(){
+    const input = document.getElementById('button')
+    input.addEventListener("click", function(){
+        alert("You clicked the button");
+    })
 }
